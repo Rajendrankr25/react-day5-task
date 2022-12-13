a73f876f43ed398fae451b6782e89988fb8d68a9
Rajendrankr25 <rajendrankr25@gmail.com>
2022-12-13 09:04:03 +0530

    day5task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { Button, TextField } from '@mui/material';
 import './App.css';
+
+//Design an UI to implement the CRUD // CRUD - Create,Read,Update,Delete // Dashboard // List Users - /users // Create User - /create-user 
+// Edit User - /edit-user/:id // profile - /profile/:id // edit-profile – /edit-profile/:id
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateUser />
     </div>
   );
+}
+
+function CreateUser() {
+  return (
+    <div>
+      <h1>Create Account</h1>
+      <div className='text-box'>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+        <Button variant="contained">Submit</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
